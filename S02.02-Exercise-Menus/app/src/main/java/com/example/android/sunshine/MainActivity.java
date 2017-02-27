1c08f9af217283dbea615fe9d497043637c1c5b9
Haeyoung Kim <geekslife@gmail.com>
2017-02-27 21:41:07 +0900

    finished S02

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,6 +18,9 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -97,12 +100,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (2) Create a menu resource in res/menu/ called forecast.xml
-    // TODO (3) Add one item to the menu with an ID of action_refresh
-    // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
+    // Complete (2) Create a menu resource in res/menu/ called forecast.xml
+    // Complete (3) Add one item to the menu with an ID of action_refresh
+    // Complete (4) Set the title of the menu item to "Refresh" using strings.xml
 
-    // TODO (5) Override onCreateOptionsMenu to inflate the menu for this Activity
-    // TODO (6) Return true to display the menu
+    // Complete (5) Override onCreateOptionsMenu to inflate the menu for this Activity
 
-    // TODO (7) Override onOptionsItemSelected to handle clicks on the refresh button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+
+    // Complete (6) Return true to display the menu
+
+    // Complete (7) Override onOptionsItemSelected to handle clicks on the refresh button
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_refresh) {
+            mWeatherTextView.setText("");
+            loadWeatherData();
+            Log.v(this.getClass().getName(), "OptionsItemSelected!");
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
